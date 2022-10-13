7a59ed8449c81a2e173b4a95be6db33d22cf76ce
Shinawonk16 <shinawonk@gmail.com>
2022-10-13 13:24:15 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace chapter_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+              //Question 1
+            // int number = 13;
+            // bool Even = number %2 ==0;
+            // bool Odd =number %2 ==1;
+            // Console.WriteLine(Even);
+            // Console.WriteLine(Odd);
+
+            //question 2
+            // int num = 35;
+            // bool tvs = (num%7==0 && num%5==0);
+            // Console.WriteLine(tvs);
+
+            //Question 3
+            
+            // int a = 765;
+            // double b = a/100;
+            // double d = b/10;
+            // Console.WriteLine(d);
+
+            //Question 4
+            // Console.WriteLine("enter three digit");
+            // int digit = Convert.ToInt32(Console.ReadLine());
+            // int p = digit%10;
+            // if(p==1 || p==0)
+            // Console.WriteLine("then it third bit is 1 or 0");
+            // else
+            // {
+            //     Console.WriteLine("then it third bit is not 1 or 0");
+            //}
+
+
+
+
+           // Question 5
+            // Console.WriteLine("enter parralel side and height of trapeziod");
+            // Console.WriteLine("enter side 1: ");
+            // double X = Convert.ToDouble(Console.ReadLine());
+            // Console.WriteLine("enter side 2: ");
+            // double Y = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("enter height");
+            // double Z = Convert.ToDouble(Console.ReadLine());
+            // double area  =(X+Y)*Z/2;
+            // Console.WriteLine(area);
+
+            // //Question 6
+            // Console.WriteLine("enter the length and breath of a rectangle");
+            // Console.WriteLine("enter length");
+            // double R = Convert.ToDouble(Console.ReadLine());
+            // Console.WriteLine("enter breath");
+            // double S = Convert.ToDouble(Console.ReadLine());
+            // double T =R*S;
+            // Console.WriteLine("area of of rectangle is " + T);
+            // double perimeter =2*(S+R);
+            // Console.WriteLine("perimeters os rectangle is " + perimeter);
+
+            //  Queston 7
+            // Console.Write("enter number:");
+            // int number = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("the weight of man is",number*0.17);
+
+            // //Question 8
+            // Console.WriteLine("enter point");
+            // Console.WriteLine("enter point on the Xaxis");
+            // double Xaxis = Convert.ToDouble(Console.ReadLine());
+            // Console.WriteLine("enter point on the Yaxis");
+            // double Yaxis = Convert.ToDouble(Console.ReadLine());
+            // if((Xaxis*Xaxis) + (Yaxis*Yaxis) <=25)
+            // {
+            //     Console.WriteLine("point is within the circle");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("point is not within the circle");
+
+           // }
+
+           //Question 9
+        //    Console.WriteLine("enter point");
+        //     Console.WriteLine("enter point on the Xaxis");
+        //     int Xaxis = Convert.ToInt32(Console.ReadLine());
+        //     Console.WriteLine("enter point on the Yaxis");
+        //     int Yaxis = Convert.ToInt32(Console.ReadLine());
+        //     if(((Xaxis*Xaxis) + (Yaxis*Yaxis)) <=25) && (((Xaxis*Xaxis) + (Yaxis*Yaxis)) <=24)
+        //     {
+        //         Console.WriteLine("point is within the circle and out of the rectangle");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("point is not within the circle and outof the rectangle");
+
+        //    }
+
+        //Question 10
+        //Console.WriteLine("enter your digit");
+
+        
+    //Question 14
+    //  int number = 72;
+    //  bool isprime = true;
+    //  if(number > 2)
+    //  for (int i = 2; i <= Math.Ceiling(Math.sqrt(number)); ++i)
+    //    {
+    //     if  (number % i == 0) isprime = false
+    //    }
+    //    Console.WriteLine("{0}");
+
+        }
+    }
+}
